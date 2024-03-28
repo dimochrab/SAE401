c13bee3ee9394f1cdc9bfc83c69b7261ce4badb1
dimochrab <ademkhdher@gmail.com>
2024-03-28 13:36:39 +0100

    Refactor routes and login functionality

--- src/Security/LoginAuthenticator.php
+++ src/Security/LoginAuthenticator.php
@@ -1,8 +1,8 @@
 <?php
+
 namespace App\Security;
 
 use Symfony\Component\HttpFoundation\RedirectResponse;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
@@ -15,71 +15,50 @@ use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordC
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 use Symfony\Component\Security\Http\SecurityRequestAttributes;
 use Symfony\Component\Security\Http\Util\TargetPathTrait;
-use App\Entity\Utilisateur;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
-
-
 
 class LoginAuthenticator extends AbstractLoginFormAuthenticator
 {
     use TargetPathTrait;
-    public const LOGIN_ROUTE = 'app_login'; // Déclaration de la constante LOGIN_ROUTE
 
+    public const LOGIN_ROUTE = 'app_login';
 
-    private EntityManagerInterface $entityManager;
-    private UserPasswordHasherInterface $passwordEncoder;
-
-    public function __construct(
-        private UrlGeneratorInterface $urlGenerator,
-        EntityManagerInterface $entityManager,
-        UserPasswordHasherInterface $passwordEncoder
-    ) {
-        $this->entityManager = $entityManager;
-        $this->passwordEncoder = $passwordEncoder;
+    public function __construct(private UrlGeneratorInterface $urlGenerator)
+    {
     }
-
     public function authenticate(Request $request): Passport
     {
-        $email = $request->request->get('email', '');
-        $user = $this->getUserByEmail($email);
-
-        if (!$user || !$this->passwordEncoder->isPasswordValid($user, $request->request->get('password'))) {
-            throw new CustomAuthenticationException('Invalid credentials');
-        }
-
-        $request->getSession()->set(SecurityRequestAttributes::LAST_USERNAME, $email);
-
+        // Changez cette ligne pour utiliser 'username' au lieu de 'email'
+        $username = $request->request->get('username', ''); // Récupère le username depuis le champ 'username'
+    
+        $request->getSession()->set(SecurityRequestAttributes::LAST_USERNAME, $username);
+    
         return new Passport(
-            new UserBadge($email, function ($email) use ($user) {
-                return $user;
-            }),
-            new PasswordCredentials($request->request->get('password')),
+            new UserBadge($username),
+            new PasswordCredentials($request->request->get('password', '')),
             [
                 new CsrfTokenBadge('authenticate', $request->request->get('_csrf_token')),
                 new RememberMeBadge(),
             ]
         );
     }
+    
+
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
     {
         if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
-            // Si une cible de redirection est définie, redirigez l'utilisateur vers cette cible
             return new RedirectResponse($targetPath);
         }
-        // Redirigez l'utilisateur vers la page d'accueil
+
+        // For example:
         return new RedirectResponse($this->urlGenerator->generate('app_home'));
+        //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
+
+
 
     protected function getLoginUrl(Request $request): string
     {
         return $this->urlGenerator->generate(self::LOGIN_ROUTE);
-    }
-
-    // Méthode pour récupérer l'utilisateur par email
-    private function getUserByEmail(string $email): ?Utilisateur
-    {
-        return $this->entityManager->getRepository(Utilisateur::class)->findOneBy(['Email' => $email]);
     }
 }
