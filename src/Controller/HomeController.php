e7e05cc0d8b6a5e01c90fe112c24edb2a34b7808
dimochrab <ademkhdher@gmail.com>
2024-03-29 18:51:25 +0100

    Refactor HomeController to display publications with file type

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,15 +4,32 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use App\Repository\PublicationRepository;
 use Symfony\Component\Routing\Attribute\Route;
+use app\Entity\Publication;
 
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function index(): Response
+    public function index(PublicationRepository $publicationRepository): Response
     {
+        $publications = $publicationRepository->findBy([], ['DateTime' => 'DESC']);
+        $user = $this->getUser();
+        
+        foreach ($publications as $publication) {
+            $filename = $publication->getPostContent();
+            $extension = pathinfo($filename, PATHINFO_EXTENSION);
+            if (in_array(strtolower($extension), ['jpg', 'jpeg', 'png', 'gif'])) {
+                $publication->fileType = 'image';
+            } elseif (in_array(strtolower($extension), ['mp4', 'avi', 'mov'])) {
+                $publication->fileType = 'video';
+            } else {
+                $publication->fileType = 'unknown';
+            }
+        }
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'publications' => $publications,
         ]);
     }
 }
